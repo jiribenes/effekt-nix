7995081bfdd1a51764300f61abe62f69ba596dde
Jiří Beneš <mail@jiribenes.com>
2024-08-13 16:59:25 +0200

    Convert the project to use Nix flakes (#2)

--- README.md
+++ README.md
@@ -1,42 +1,214 @@
 # effekt-nix
 
 > [!NOTE]
-> Contributions are very welcome, see TODOs below! :)
+> Contributions are very welcome, see Contributing section below! :)
 
-A simple Nix package for the [Effekt programming language](https://github.com/effekt-lang/effekt).
+A comprehensive Nix flake for the [Effekt programming language](https://github.com/effekt-lang/effekt).
 
-## Installation
+## Quick Start
 
-```sh
-nix-build -A effekt
+<details>
+  <summary><b>I want to quickly run Effekt!</b></summary>
+
+  Great! Here's how you run the latest released version of Effekt:
+  ```sh
+  # run Effekt REPL
+  nix run github:jiribenes/effekt-nix
+
+  # run the latest version of the Effekt compiler on a file (with default backend)
+  nix run github:jiribenes/effekt-nix -- file.effekt
+
+  # run the latest version of the Effekt compiler on a file with the LLVM backend
+  nix run github:jiribenes/effekt-nix -- --backend llvm file.effekt
+
+  # run a specific version of the Effekt compiler
+  nix run github:jiribenes/effekt-nix#effekt_0_2_2 -- --help
+  ```
+
+</details>
+
+---
+
+<details>
+  <summary><b>I want to quickly play with Effekt!</b></summary>
+
+  Sure, let's get you a devshell in which you can just call `effekt` then:
+  ```sh
+  # a shell with the latest Effekt version
+  nix develop github:jiribenes/effekt-nix
+
+  # a shell with a specific Effekt version
+  nix develop github:jiribenes/effekt-nix#effekt_0_2_2
+
+  # ADVANCED: a shell for developing the Effekt compiler
+  nix develop github:jiribenes/effekt-nix#compilerDev
+  ```
+
+  You can use this -- for example -- for benchmarking or for working with LSP support in VSCode.
+
+</details>
+
+---
+
+<details>
+  <summary><b>I want to quickly install Effekt on my machine!</b></summary>
+
+  Alright, let's install Effekt on your machine so that you can call `effekt` at any time:
+  ```sh
+  # install latest version of Effekt
+  nix profile install github:jiribenes/effekt-nix
+  ```
+
+</details>
+
+---
+
+<details>
+  <summary><b>I want to quickly build Effekt on my machine!</b></summary>
+
+  _... okay, I guess? ..._
+  ```sh
+  # builds the latest version of Effekt
+  nix build github:jiribenes/effekt-nix
+  ```
+
+  The result of the build is in the `result/` folder (the binary is in `result/bin/`).
+
+</details>
+
+## Examples of using this Nix Flake
+
+### Adding a devshell with Effekt to your own Nix flake
+
+#### Latest released version of Effekt:
+
+```nix
+{
+  inputs.effekt-nix.url = "github:jiribenes/effekt-nix";
+  
+  outputs = { self, nixpkgs, effekt-nix }:
+    let
+      system = "x86_64-linux";
+    in {
+      devShell = effekt-nix.devShells.${system}.default;
+    };
+}
 ```
 
-then call `./result/bin/effekt` to invoke the Effekt compiler/interpreter.
+#### Specific released version of Effekt:
 
-## Usage
-
-If you just want a temporary shell with the Effekt compiler/interpreter inside, use the command:
-
-```sh
-nix-shell
+```nix
+{
+  inputs.effekt-nix.url = "github:jiribenes/effekt-nix";
+  
+  outputs = { self, nixpkgs, effekt-nix }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      effekt-lib = effekt-nix.lib.${system};
+    in {
+      devShell = effekt-lib.mkDevShell {
+        effektVersion = "0.2.2";
+      };
+    };
+}
 ```
 
-This will spawn a new shell in which one can just call `effekt`.
+### Building an app written in Effekt in your Nix flake
 
-### WIP
+```nix
+{
+  inputs.effekt-nix.url = "github:jiribenes/effekt-nix";
+  
+  outputs = { self, nixpkgs, effekt-nix }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      effekt-lib = effekt-nix.lib.${system};
+    in {
+      packages.default = effekt-lib.buildEffektPackage {
+        pname = "my-effekt-project";
+        version = "1.0.0";
+        src = ./.;              # Path to your Effekt project
+        main = ./main.effekt;   # the main Effekt file to run
+        effektVersion = "0.2.2";
+        effektBackends = with effekt-lib.effektBackends; [ js llvm ];
+      };
+    };
+}
+```
 
-You can also use `nix-build -A effekt-nightly` to build directly from a **relative** Effekt source folder (needs setup in `default.nix`).
-Similarly you can use `nix-shell -A shell-nightly default.nix` to get a shell with `Effekt` from a relative folder.
+Here's a breakdown of `buildEffektPackage`'s arguments:
 
-You can also use `nix-shell -A devshell default.nix` to get the development environment necessary for developing the Effekt compiler itself.
+- `pname`: The name of your package.
+- `version`: The version of your package.
+- `src`: The source directory of your Effekt project.
+- `main`: The main Effekt file to compile.
+- `tests`: (Optional) A list of test files to run during the build process.
+- `effekt`: (Optional) A specific Effekt derivation to use. If not provided, it uses the version specified by `effektVersion`.
+- `effektVersion`: The version of Effekt to use (defaults to the latest version).
+- `effektBackends`: A list of backends to compile your project with. The first backend in the list is considered the default.
+- `buildInputs`: (Optional) Additional build inputs required for your package.
 
-## TODO
+The function will compile your project with all specified backends and create a binary for each.
+It also sets up a symbolic link to the default backend's binary under the `pname`.
 
-- [x] provide a Nix shell with Effekt as an input
-- [ ] add configurable pulling in Chez Scheme and/or LLVM
-- [x] building directly from a folder
-- [ ] building directly from a GitHub commit
-- [ ] Flake-ify!
-- [ ] upstream the `effekt` package itself to Nixpkgs
-- [ ] Docker images
-- [ ] CI
+#### Using a custom Effekt compiler build
+
+```nix
+{
+  inputs.effekt-nix.url = "github:jiribenes/effekt-nix";
+  
+  outputs = { self, nixpkgs, effekt-nix }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      effekt-lib = effekt-nix.lib.${system};
+      
+      # Define your own Effekt build from source:
+      myCustomEffekt = effekt-lib.buildEffektFromSource {
+        # ... by defining the path to your compiler source here:
+        src = ./path/to/effekt/compiler/source;
+        backends = with effekt-lib.effektBackends; [ js llvm ];
+      };
+    in {
+      devShell = effekt-lib.mkDevShell {
+        effekt = myCustomEffekt;
+      };
+      
+      packages.myPackage = effekt-lib.buildEffektPackage {
+        pname = "my-custom-effekt-project";
+        version = "1.0.0";
+        src = ./.; # Path to your Effekt project
+        main = ./main.effekt;
+        effekt = myCustomEffekt; #
+      };
+    };
+}
+```
+
+### Available Backends
+
+The `effekt-nix` flake supports the following backends:
+
+- `js`: JavaScript backend (always available)
+- `llvm`: LLVM backend
+- `chez-callcc`: Chez Scheme backend with call/cc
+- `chez-monadic`: Chez Scheme backend with monadic style
+- `chez-lift`: Chez Scheme backend with lifting
+- `ml`: MLton backend (only available on systems that support MLton)
+
+You can specify which backends to use when building an Effekt package or creating a development shell.
+
+## Contributing
+
+Contributions of all kinds are very welcome, feel free to create a PR.
+
+A common chore is updating this repo with released versions of Effekt in `releases.json` (hopefully will be addressed with CI).
+
+### Adding a new Effekt version
+
+To add support for a new Effekt version:
+
+1. Update the `releases.json` file with the new version number and its corresponding SHA256 hash.
+2. The flake will automatically generate new packages and development shells for the added version.
---
+++ flake.nix
@@ -0,0 +1,287 @@
+{
+  description = "Nix interop for the Effekt programming language";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    sbt-derivation = {
+      url = "github:zaninime/sbt-derivation";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, sbt-derivation }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        # Load Effekt versions and their corresponding SHA256 hashes from 'releases.json'
+        # If you want to add a new release version, just add it there.
+        effektVersions = builtins.fromJSON (builtins.readFile ./releases.json);
+
+        # Gets the newest version from 'effektVersions'
+        latestVersion = builtins.head (builtins.sort (a: b: builtins.compareVersions a b > 0) (builtins.attrNames effektVersions));
+
+        # Checks if MLton is available for the current system
+        isMLtonSupported = builtins.elem system pkgs.mlton.meta.platforms;
+
+        # Available backends for Effekt, depending on the current 'system'
+        effektBackends = {
+          js = {
+            name = "js";
+            buildInputs = [pkgs.nodejs];
+          };
+          llvm = {
+            name = "llvm";
+            buildInputs = [pkgs.llvm pkgs.libuv pkgs.clang]; # GCC is also usable here
+          };
+          chez-callcc = {
+            name = "chez-callcc";
+            buildInputs = [pkgs.chez];
+          };
+          chez-monadic = {
+            name = "chez-monadic";
+            buildInputs = [pkgs.chez];
+          };
+          chez-lift = {
+            name = "chez-lift";
+            buildInputs = [pkgs.chez];
+          };
+        } // pkgs.lib.optionalAttrs isMLtonSupported {
+          ml = {
+            name = "ml";
+            buildInputs = [pkgs.mlton];
+          };
+        };
+
+        # Meta information about the Effekt programming language
+        effektMeta = {
+          mainProgram = "effekt";
+          description = "A research language with effect handlers and lightweight effect polymorphism";
+          homepage = "https://effekt-lang.org/";
+          license = pkgs.lib.licenses.mit;
+        };
+
+        # Creates an Effekt derivation from a prebuilt GitHub release
+        buildEffektRelease = {
+          version,
+          sha256,
+          backends ? [effektBackends.js]
+        }:
+          assert backends != []; # Ensure at least one backend is specified
+          pkgs.stdenv.mkDerivation {
+            pname = "effekt";
+            inherit version;
+
+            src = pkgs.fetchurl {
+              url = "https://github.com/effekt-lang/effekt/releases/download/v${version}/effekt.tgz";
+              inherit sha256;
+            };
+
+            nativeBuildInputs = [pkgs.makeWrapper];
+            buildInputs = [pkgs.jre] ++ pkgs.lib.concatMap (b: b.buildInputs) backends;
+
+            installPhase = ''
+              mkdir -p $out/bin $out/lib
+              mv bin/effekt $out/lib/effekt.jar
+              mv libraries $out/libraries
+
+              makeWrapper ${pkgs.jre}/bin/java $out/bin/effekt \
+                --add-flags "-jar $out/lib/effekt.jar" \
+                --prefix PATH : ${pkgs.lib.makeBinPath (pkgs.lib.concatMap (b: b.buildInputs) backends)}
+            '';
+
+            meta = effektMeta;
+          };
+
+        # Creates an Effekt derivation by building Effekt from (some) source
+        buildEffektFromSource = {
+          src,
+          version,
+          depsSha256, # SHA256 of the Scala dependencies
+          backends ? [effektBackends.js],
+        }:
+          assert backends != []; # Ensure at least one backend is specified
+          sbt-derivation.lib.mkSbtDerivation {
+            inherit pkgs;
+            pname = "effekt";
+            inherit version;
+            inherit src;
+
+            nativeBuildInputs = [pkgs.nodejs pkgs.maven pkgs.makeWrapper pkgs.gnused];
+            buildInputs = [pkgs.jre] ++ pkgs.lib.concatMap (b: b.buildInputs) backends;
+
+            inherit depsSha256;
+            depsArchivalStrategy = "copy";
+            depsWarmupCommand = ''
+              echo "Warming up: getting compiler bridge thingy"
+              sbt scalaCompilerBridgeBinaryJar
+              echo "Warming up: updating"
+              sbt update
+              echo "Warming up: FINISHED"
+            '';
+
+            # Change the version in build.sbt
+            prePatch = ''
+              sed -i 's/lazy val effektVersion = "[^"]*"/lazy val effektVersion = "${version}"/' build.sbt
+            '';
+
+            buildPhase = ''
+              export MAVEN_OPTS="-Dmaven.repo.local=$out/.m2/repository"
+              sbt assembleBinary
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin $out/lib
+              mv bin/effekt $out/lib/effekt.jar
+              mv libraries $out/libraries
+
+              makeWrapper ${pkgs.jre}/bin/java $out/bin/effekt \
+                --add-flags "-jar $out/lib/effekt.jar" \
+                --prefix PATH : ${pkgs.lib.makeBinPath (pkgs.lib.concatMap (b: b.buildInputs) backends)}
+            '';
+
+            meta = effektMeta;
+          };
+
+        # Builds an Effekt package
+        buildEffektPackage = pkgs.lib.makeOverridable (
+          {
+            pname,                                # package name
+            version,                              # package version
+            src,                                  # source of the package
+            main,                                 # (relative) path to the entrypoint
+            tests ? [],                           # (relative) paths to the tests
+            effekt ? null,                        # the explicit Effekt derivation to use: uses latest release if not set
+            effektVersion ? latestVersion,        # the Effekt version to use
+            effektBackends ? [effektBackends.js], # Effekt backends to use -- first backend is the "default" one
+            buildInputs ? [],                     # other build inputs required for the package
+          }:
+            assert effektBackends != []; # Ensure at least one backend is specified
+            let
+              defaultBackend = builtins.head effektBackends;
+              effektBuild = if effekt != null then effekt else buildEffektRelease {
+                version = effektVersion;
+                sha256 = effektVersions.${effektVersion};
+                backends = effektBackends;
+              };
+            in
+            pkgs.stdenv.mkDerivation {
+              inherit pname version src;
+
+              nativeBuildInputs = [effektBuild];
+              inherit buildInputs;
+
+              buildPhase = ''
+                mkdir -p out
+                ${pkgs.lib.concatMapStrings (backend: ''
+                  effekt --build --backend ${backend.name} ${main}
+                  mv out/$(basename ${main}) out/${pname}-${backend.name}
+                '') effektBackends}
+              '';
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp -r out/* $out/bin/
+                ln -s $out/bin/${pname}-${defaultBackend.name} $out/bin/${pname}
+              '';
+
+              checkPhase = pkgs.lib.concatMapStrings (test:
+                pkgs.lib.concatMapStrings (backend: ''
+                echo "Running test ${test} with backend ${backend.name}"
+                effekt --backend ${backend.name} ${test}
+              '') effektBackends
+            ) tests;
+
+            doCheck = tests != [];
+
+            # Entry point is the program called ${pname}
+            meta.mainProgram = pname;
+          }
+        );
+
+        # Creates a dev-shell for an Effekt package / version & backends
+        mkDevShell = {
+          effekt ? null,
+          effektVersion ? latestVersion,
+          backends ? [effektBackends.js]
+        }:
+          let
+            effektBuild = if effekt != null then effekt else buildEffektRelease {
+              version = effektVersion;
+              sha256 = effektVersions.${effektVersion};
+              inherit backends;
+            };
+          in
+          pkgs.mkShell {
+            buildInputs = [effektBuild] ++ pkgs.lib.concatMap (b: b.buildInputs) backends;
+          };
+
+        # Development shell for Effekt compiler development
+        compilerDevShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            sbt jre maven scala_3
+          ] ++ pkgs.lib.concatMap (b: b.buildInputs) (builtins.attrValues effektBackends);
+        };
+
+        # Automatically generated packages for all 'effektVersions' with all backends
+        autoPackages = pkgs.lib.mapAttrs' (version: _:
+          pkgs.lib.nameValuePair "effekt_${builtins.replaceStrings ["."] ["_"] version}" (
+            buildEffektRelease {
+              inherit version;
+              sha256 = effektVersions.${version};
+              backends = builtins.attrValues effektBackends;
+            }
+          )
+        ) effektVersions;
+
+        # Automatically generated devshells for all 'effektVersions' with all backends
+        autoDevShells = pkgs.lib.mapAttrs' (version: _:
+          pkgs.lib.nameValuePair "effekt_${builtins.replaceStrings ["."] ["_"] version}" (
+            mkDevShell {
+              effektVersion = version;
+              backends = builtins.attrValues effektBackends;
+            }
+          )
+        ) effektVersions;
+
+        # Quick alias for the latest pre-built Effekt derivation
+        latestEffekt = autoPackages."effekt_${builtins.replaceStrings ["."] ["_"] latestVersion}";
+      in {
+        # Helper functions and types for external use
+        lib = {
+          inherit buildEffektRelease buildEffektFromSource buildEffektPackage mkDevShell effektBackends isMLtonSupported;
+        };
+
+        # Automatically generated packages + latest version (as default)
+        packages = autoPackages // {
+          default = latestEffekt;
+        };
+
+        # Development shells
+        devShells = autoDevShells // {
+          default = mkDevShell {
+            effektVersion = latestVersion;
+            backends = builtins.attrValues effektBackends;
+          };
+          compilerDev = compilerDevShell;
+        };
+
+        # Ready-to-run applications
+        apps = {
+          default = flake-utils.lib.mkApp {
+            drv = latestEffekt;
+            name = "effekt";
+          };
+        } // builtins.mapAttrs (name: pkg:
+          flake-utils.lib.mkApp {
+            drv = pkg;
+            name = "effekt";
+          }
+        ) autoPackages;
+
+        checks = { };
+      }
+    );
+}
